2d82909d05bee093f5fed39e033f1c5ff7757fab
Fedi Mhenni <fedimhenni19@gmail.com>
2026-07-07 16:40:25 +0200

    feat: premier rendu fonctionnel (framework maison + Content-Type Projet + consommation API Strapi)

---
+++ frontend/index.js
@@ -0,0 +1,5 @@
+import BrowserRouter from "./components/router/browser-router.js";
+import routes from "./routes/index.js";
+
+const rootElement = document.getElementById("root");
+BrowserRouter(rootElement, routes);
