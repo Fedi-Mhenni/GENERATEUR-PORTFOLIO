d907c62edcad216588b1636536525229d2fbdc45
Fedi Mhenni <fedimhenni19@gmail.com>
2026-07-08 14:45:38 +0200

    refactor(vanilla-engine): réorganisation en src/ (core, router, state, validation, components, prototypes)

---
+++ packages/vanilla-engine/src/router/browser-router.js
@@ -0,0 +1,39 @@
+import generateStructure from "../core/generate-structure.js";
+
+export default function BrowserRouter(rootElement, routes) {
+  async function refreshPage() {
+    const pathname = window.location.pathname;
+    const generator = routes[pathname] ?? routes["*"];
+    const structure = await generator();
+
+    if (rootElement.childNodes[0]) {
+      rootElement.replaceChild(
+        generateStructure(structure),
+        rootElement.childNodes[0],
+      );
+    } else {
+      rootElement.appendChild(generateStructure(structure));
+    }
+  }
+  window.addEventListener("popstate", refreshPage);
+  window.addEventListener("pushstate", refreshPage);
+  refreshPage();
+}
+
+export function BrowserLink(url, title) {
+  return {
+    type: "a",
+    attributes: [["href", url]],
+    children: [title],
+    events: [
+      [
+        "click",
+        (event) => {
+          event.preventDefault();
+          window.history.pushState({}, undefined, url);
+          window.dispatchEvent(new Event("pushstate"));
+        },
+      ],
+    ],
+  };
+}
