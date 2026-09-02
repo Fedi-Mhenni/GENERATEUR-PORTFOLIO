7bb1e0d8360b001455c279d4f0d51e176c940560
Aijing LI <renoxevans@gmail.com>
2026-09-02 18:54:32 +0200

    feat(infra): use production Strapi API URLs

--- sites/site-aijing/config.js
+++ sites/site-aijing/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_ORIGIN: "http://localhost:1337",
-  API_URL: "http://localhost:1337/api",
+  API_ORIGIN: "https://api-portfolio-aijing.aijing.li",
+  API_URL: "https://api-portfolio-aijing.aijing.li/api",
 };
 
 export default config;
