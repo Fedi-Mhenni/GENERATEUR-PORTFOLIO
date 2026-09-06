7d3ab18df0f1f04e84e94bbd5d19f7ddd282700c
Aijing LI <renoxevans@gmail.com>
2026-09-06 18:41:12 +0200

    fix(site-aijing): api url config

--- sites/site-aijing/config.js
+++ sites/site-aijing/config.js
@@ -1,6 +1,13 @@
 const config = {
-  API_ORIGIN: "http://localhost:1337", // http://localhost:1337， https://api-portfolio-aijing.aijing.li
-  API_URL: "http://localhost:1337/api",
+  API_ORIGIN: "http://api-portfolio-aijing.aijing.li",
+  API_URL: "https://api-portfolio-aijing.aijing.li/api",
 };
 
+/*
+const config = {
+    API_ORIGIN: "http://localhost:1337",
+    API_URL: "http://localhost:1337/api",
+};*/
+
 export default config;
+
